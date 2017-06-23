2decf23d5a26d8ee2e076c737a6a9b891a524bd6
ankvel <ankvel@gmail.com>
2017-06-23 11:43:12 +0300

    Added base project files.

---
+++ src/main/java/ankvel/edu/arclang/ArclangApplication.java
@@ -0,0 +1,12 @@
+package ankvel.edu.arclang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArclangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArclangApplication.class, args);
+	}
+}
